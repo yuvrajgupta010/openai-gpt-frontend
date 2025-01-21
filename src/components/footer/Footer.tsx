ab2e41a85583e9c39223da690d2bce9dfbf2ed5d
Yuvraj Gupta <yuvrajgupta036@gmail.com>
2025-01-21 13:01:08 +0530

    openai gpt ui

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const currentYear = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Copyright © <span id="year">{currentYear}</span> Built With love by
+          <span> Yuvraj Gupta</span>
+          💘
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/helpers/api-communicator.ts
@@ -0,0 +1,90 @@
+import axios from "axios";
+export const loginUser = async (email: string, password: string) => {
+  const res = await axios.post("/user/login", { email, password });
+  if (res.status !== 200) {
+    throw new Error("Unable to login");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const signupUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/user/signup", { name, email, password });
+  if (res.status !== 201) {
+    throw new Error("Unable to Signup");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const checkAuthStatus = async () => {
+  const res = await axios.get("/user/auth-status");
+  if (res.status !== 200) {
+    throw new Error("Unable to authenticate");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const sendChatRequest = async (message: string) => {
+  const response = await fetch("http://localhost:5000/api/v1/chat/new", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ message }),
+  });
+
+  if (response.status !== 200) {
+    throw new Error("Unable to send chat");
+  }
+
+  if (!response.body) {
+    throw new Error("No response body received.");
+  }
+
+  const reader = response.body.getReader();
+  return reader;
+
+  ///////////////////////////////////////////////////
+  // previous
+  //////////////////////////////////////////////////
+  // const res = await axios.post("/chat/new", { message });
+  // if (res.status !== 200) {
+  //   throw new Error("Unable to send chat");
+  // }
+  // const data = await res.data;
+  // return data;
+};
+
+export const getUserChats = async () => {
+  const res = await axios.get("/chat/all-chats");
+  if (res.status !== 200) {
+    throw new Error("Unable to send chat");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const deleteUserChats = async () => {
+  const res = await axios.delete("/chat/delete");
+  if (res.status !== 200) {
+    throw new Error("Unable to delete chats");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const logoutUser = async () => {
+  const res = await axios.get("/user/logout");
+  if (res.status !== 200) {
+    throw new Error("Unable to delete chats");
+  }
+  const data = await res.data;
+  return data;
+};
